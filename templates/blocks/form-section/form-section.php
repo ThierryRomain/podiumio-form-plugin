80552e0f2f105ae818872647621cc8d6859baf18
Thierry R. Le Bourdais <thierryr@orphic.ca>
2020-05-27 10:44:05 -0400

    added preview image to form

--- templates/blocks/form-section/form-section.php
+++ templates/blocks/form-section/form-section.php
@@ -3,26 +3,28 @@ $title = get_field('title');
 $shortcode = get_field('shortcode');
 $preForm = get_field('pre_form');
 $postForm = get_field('post_form');
+$img = get_field('img');
 ?>
 
 <div class="arrive-on-scroll">
-    <div class="w-container">
-        <div class="os-form-section" style="margin-top:10px;">
-            <div class="os-form-section-question-wrapper">
-                <h4><?php echo $title ?></h4>
-                <div class="os-form-section-arrow-wrapper">
-                    <i class="fas fa-angle-down os-form-section-arrow"></i>
-                </div>
+    <div class="os-form-section" style="margin-top:10px;">
+        <div class="os-form-section-question-wrapper">
+            <h4><?php echo $title ?></h4>
+            <div class="os-form-section-arrow-wrapper">
+                <i class="fas fa-angle-down os-form-section-arrow"></i>
             </div>
-            <?php if(is_admin()): ?>
-                <div class="os-form-section-answer-panel" style="display:block;">
-            <?php else: ?>
-                <div class="os-form-section-answer-panel">
-            <?php endif; ?>
-                <?php echo $preForm ?>
-                <?php echo do_shortcode($shortcode);  ?>
-                <?php echo $postForm ?>
-                </div>
         </div>
+        <?php if(is_admin()): ?>
+            <div class="os-form-section-answer-panel" style="display:block;">
+        <?php else: ?>
+            <div class="os-form-section-answer-panel">
+        <?php endif; ?>
+            <?php if($img): ?>
+                <img src="<?php echo $img; ?>" class="os-form-section-preview" />
+            <?php endif; ?>
+            <div style="margin-top:20px;margin-bottom:20px;"><?php echo $preForm ?></div>
+            <?php echo do_shortcode($shortcode);  ?>
+            <div style="margin-top:20px;margin-bottom:20px;"><?php echo $postForm ?></div>
+            </div>
     </div>
 </div>
